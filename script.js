a3b96ca2516f268c063798826b6e68e486110cdc
Saud <121959804+saudshah207@users.noreply.github.com>
2024-07-03 16:45:29 +0500

    Run 5 rounds per game & handle input cancellation
    Player can now play five rounds in a single game,
    further if user cancels the input prompt, the
    game is aborted.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let computerScore = 0,
-  humanScore = 0;
-
 function getComputerChoice() {
   // Get random number between 0 and 2 (both inclusive)
   let randomNum = Math.floor(Math.random() * 3);
@@ -15,12 +12,14 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
+function getHumanChoice(currentRound) {
   // Ask user/human for their choice
-  let humanChoice = prompt("Enter either Rock, Paper or Scissors");
+  let humanChoice = prompt(
+    `Round ${currentRound} :\nEnter either Rock, Paper or Scissors`
+  );
 
-  // Only return human's choice if it's not null or empty string
-  if (humanChoice) {
+  // Only proceed if human's choice is not null
+  if (humanChoice !== null) {
     // Standardize human input so that only first character is capital
     humanChoice =
       humanChoice.slice(0, 1).toUpperCase() +
@@ -38,96 +37,128 @@ function getHumanChoice() {
   }
 }
 
-function playRound(computerChoice, humanChoice) {
-  /* Following are only possible pairs in a
-    game of Rock, Paper and Scissors :
+function playGame() {
+  let computerScore = 0,
+    humanScore = 0,
+    gameRound,
+    continueGame = true;
 
-    Rock > Scissors
-    Scissors > Paper
-    Paper > Rock
-    Rock === Rock
-    Scissors === Scissors
-    Paper === Paper
-    
-  */
-
-  // Abort if humanChoice contains undefined
-  if (!humanChoice) {
-    return;
-  }
-
-  // Out of Rock & Scissors, the player with Rock wins
-  if (
-    (computerChoice === "Rock" || humanChoice === "Rock") &&
-    (computerChoice === "Scissors" || humanChoice === "Scissors")
-  ) {
-    if (humanChoice === "Rock") {
-      console.log(
-        `You chose ${humanChoice} | Computer chose ${computerChoice}`
-      );
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-      console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+  // Each game should consist of 5 rounds
+  for (gameRound = 1; gameRound <= 5; gameRound++) {
+    // If user cancels input prompt, stop
+    if (continueGame) {
+      playRound(getComputerChoice(), getHumanChoice(gameRound));
+      if (gameRound === 5) {
+        if (computerScore > humanScore) {
+          console.log("Computer wins this game! 🖥🎉");
+        } else if (computerScore === humanScore) {
+          console.log("This game is a tie! 🏳");
+        } else {
+          console.log("Human wins this game! 👤🎊");
+        }
+      }
     } else {
-      console.log(
-        `You chose ${humanChoice} | Computer chose ${computerChoice}`
-      );
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-      computerScore++;
-      console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      break;
     }
   }
 
-  // Out of Scissors & Paper, the player with Scissors wins
-  else if (
-    (computerChoice === "Scissors" || humanChoice === "Scissors") &&
-    (computerChoice === "Paper" || humanChoice === "Paper")
-  ) {
-    if (humanChoice === "Scissors") {
+  function playRound(computerChoice, humanChoice) {
+    /* Following are only possible pairs in a
+      game of Rock, Paper and Scissors :
+  
+      Rock > Scissors
+      Scissors > Paper
+      Paper > Rock
+      Rock === Rock
+      Scissors === Scissors
+      Paper === Paper
+      
+    */
+
+    // Abort if humanChoice contains undefined
+    if (!humanChoice) {
+      continueGame = false;
+      return continueGame;
+    }
+
+    // Declare Round number
+    console.log(`%cRound ${gameRound} : `, "font-weight: bold; font-size: 13px");
+
+    // Out of Rock & Scissors, the player with Rock wins
+    if (
+      (computerChoice === "Rock" || humanChoice === "Rock") &&
+      (computerChoice === "Scissors" || humanChoice === "Scissors")
+    ) {
+      if (humanChoice === "Rock") {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      } else {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      }
+    }
+
+    // Out of Scissors & Paper, the player with Scissors wins
+    else if (
+      (computerChoice === "Scissors" || humanChoice === "Scissors") &&
+      (computerChoice === "Paper" || humanChoice === "Paper")
+    ) {
+      if (humanChoice === "Scissors") {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You win! ${humanChoice} beat ${computerChoice}`);
+        humanScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      } else {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
+        computerScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      }
+    }
+
+    // Out of Paper & Rock, the player with Paper wins
+    else if (
+      (computerChoice === "Paper" || humanChoice === "Paper") &&
+      (computerChoice === "Rock" || humanChoice === "Rock")
+    ) {
+      if (humanChoice === "Paper") {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      } else {
+        console.log(
+          `You chose ${humanChoice} | Computer chose ${computerChoice}`
+        );
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+        console.log(`You : ${humanScore} | Computer : ${computerScore}`);
+      }
+    }
+
+    // Otherwise, the players have chosen same object & it's a tie
+    else {
       console.log(
         `You chose ${humanChoice} | Computer chose ${computerChoice}`
       );
-      console.log(`You win! ${humanChoice} beat ${computerChoice}`);
-      humanScore++;
-      console.log(`You : ${humanScore} | Computer : ${computerScore}`);
-    } else {
-      console.log(
-        `You chose ${humanChoice} | Computer chose ${computerChoice}`
-      );
-      console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
-      computerScore++;
+      console.log("It's a tie!");
       console.log(`You : ${humanScore} | Computer : ${computerScore}`);
     }
-  }
-
-  // Out of Paper & Rock, the player with Paper wins
-  else if (
-    (computerChoice === "Paper" || humanChoice === "Paper") &&
-    (computerChoice === "Rock" || humanChoice === "Rock")
-  ) {
-    if (humanChoice === "Paper") {
-      console.log(
-        `You chose ${humanChoice} | Computer chose ${computerChoice}`
-      );
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-      console.log(`You : ${humanScore} | Computer : ${computerScore}`);
-    } else {
-      console.log(
-        `You chose ${humanChoice} | Computer chose ${computerChoice}`
-      );
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-      computerScore++;
-      console.log(`You : ${humanScore} | Computer : ${computerScore}`);
-    }
-  }
-
-  // Otherwise, the players have chosen same object & it's a tie
-  else {
-    console.log(`You chose ${humanChoice} | Computer chose ${computerChoice}`);
-    console.log("It's a tie!");
-    console.log(`You : ${humanScore} | Computer : ${computerScore}`);
   }
 }
 
-playRound(getComputerChoice(), getHumanChoice());
+playGame();
